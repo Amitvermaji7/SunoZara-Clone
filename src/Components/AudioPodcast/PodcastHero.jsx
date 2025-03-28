9b56f33ef5eba5cdea65874b4afa942bf7e19dbb
AMIT KUMAR VERMA <vermajiamit26@gmail.comm>
2025-03-29 01:42:11 +0530

    Updated podcast page with separate routing

--- src/Components/AudioPodcast/PodcastHero.jsx
+++ src/Components/AudioPodcast/PodcastHero.jsx
@@ -1,9 +1,130 @@
-import React from "react";
+import React, { useRef, useState } from 'react';
+import SongCard from '../SongCard';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function PodcastHero() {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import '../AudioPodcast/Hero.css';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+export default function App() {
   return (
-    <div className="h-[50vh] flex items-center justify-center bg-blue-700 text-white text-3xl">
-      Welcome to the Podcast Hero Section!
+    <div className="bg-[#080B12] py-12 overflow-hidden
+    w-full ">
+      
+      <Swiper
+        
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch:0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+             <SongCard
+                       description="this is the des"
+                       title="title 1"
+                       src="https://swiperjs.com/demos/images/nature-1.jpg"
+                       sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+             />
+        </SwiperSlide>
+       
+        <SwiperSlide>
+             <SongCard
+                      description="description 2"
+                      title="title 2"
+                      src="https://swiperjs.com/demos/images/nature-2.jpg"
+                      sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"
+                    />
+         </SwiperSlide>
+       
+         <SwiperSlide>
+          <SongCard
+                    description="this is the des"
+                    title="title 1"
+                    src="https://swiperjs.com/demos/images/nature-3.jpg"
+                    sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"
+                  />
+        </SwiperSlide>
+       
+        <SwiperSlide>
+            <SongCard
+                     description="description 2"
+                     title="title 2"
+                     src="https://swiperjs.com/demos/images/nature-4.jpg"
+                     sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3"
+                   />
+
+        </SwiperSlide>
+        
+        <SwiperSlide>
+          <SongCard
+                    description="this is the des"
+                    title="title 1"
+                    src="https://swiperjs.com/demos/images/nature-5.jpg"
+                    sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3"
+                  />
+        </SwiperSlide>
+        
+        <SwiperSlide>
+          {/* <img src="https://swiperjs.com/demos/images/nature-6.jpg" /> */}
+
+          <SongCard
+                    description="description 2"
+                    title="title 2"
+                    src="https://swiperjs.com/demos/images/nature-6.jpg"
+                    sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3"
+                  />
+        </SwiperSlide>
+       
+        <SwiperSlide>
+          {/* <img src="https://swiperjs.com/demos/images/nature-7.jpg" /> */}
+             
+              <SongCard
+                       description="this is the des"
+                       title="title 1"
+                       src="https://swiperjs.com/demos/images/nature-7.jpg"
+                       sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3"
+                     />
+
+        </SwiperSlide>
+       
+        <SwiperSlide>
+          {/* <img src="https://swiperjs.com/demos/images/nature-8.jpg" /> */}
+           <SongCard
+                    description="this is the des"
+                    title="title 1"
+                    src="https://swiperjs.com/demos/images/nature-8.jpg"
+                    sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3"
+                  />
+        </SwiperSlide>
+        
+        <SwiperSlide>
+          {/* <img src="https://swiperjs.com/demos/images/nature-9.jpg" /> */}
+
+           <SongCard
+                    description="this is the des"
+                    title="title 1"
+                    src="https://swiperjs.com/demos/images/nature-3.jpg"
+                    sample="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3"
+                  />
+        
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
--- src/Components/SongCard.jsx
+++ src/Components/SongCard.jsx
@@ -1,43 +1,51 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import play from '../assets/play.png';
 import pause from '../assets/pause.png';
 
-
 export default function SongCard({ title, description, src, sample }) {
   const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const handlePlayPause = () => {
     if (audioRef.current) {
       if (audioRef.current.paused) {
         audioRef.current.play()
-        .catch(error => {
-          console.error('Error playing audio:', error);
-        });
+          .then(() => setIsPlaying(true))
+          .catch(error => console.error('Error playing audio:', error));
       } else {
         audioRef.current.pause();
+        setIsPlaying(false);
       }
     }
   };
 
   return (
-    <div className='text-white static'>
-      <img className='rounded-lg' src={src} alt="" />
-      <button className='border-2 border-gray-100 px-4 py-1 rounded-full
- hover:bg-white hover:text-black text-gray-100 transition-all duration-300' onClick={handlePlayPause}><img src={pause ? play : pause} className="w-4 h-4" /></button>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <p>⭐⭐⭐⭐⭐</p>
+    <div className='text-white static bg-gray-900 p-4 rounded-lg shadow-lg w-64'>
+      <img className='rounded-lg w-full h-40 object-cover' src={src} alt="Song Thumbnail" />
       
+      <h1 className='mt-2 text-lg font-bold'>{title}</h1>
+      <p className='text-gray-400'>{description}</p>
+      <p className='text-yellow-400'>⭐⭐⭐⭐⭐</p>
+
+      <button 
+        className='border-2 border-gray-100 px-4 py-2 rounded-full mt-2 flex items-center 
+        hover:bg-white hover:text-black text-gray-100 transition-all duration-300' 
+        onClick={handlePlayPause}
+      >
+        <img src={isPlaying ? pause : play} className="w-4 h-4 mr-2" alt="Play/Pause" />
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+
       {sample && (
-        <audio ref={audioRef}>
+        <audio ref={audioRef} className="hidden">
           <source src={sample} type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
       )}
     </div>
   );
-};
+}
 
 SongCard.propTypes = {
   title: PropTypes.string.isRequired,
